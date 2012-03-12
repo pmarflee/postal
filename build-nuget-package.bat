5aed7e8699a7ff21dc227d6bb3c7d4cc5a71552f
Paul Marfleet <paul.marfleet@gmail.com>
2012-03-12 08:48:46 +0000

    Include dependencies that do not have NuGet packages in package build

--- build-nuget-package.bat
+++ build-nuget-package.bat
@@ -9,6 +9,9 @@ mkdir lib
 mkdir lib\net40
 
 copy ..\src\postal\bin\release\postal.dll lib\net40
+copy ..\lib\BoneSoft.CSS.dll lib\net40
+copy ..\lib\Fizzler.dll lib\net40
+copy ..\lib\Fizzler.Systems.HtmlAgilityPack.dll lib\net40
 
 ..\src\.nuget\nuget.exe pack postal.nuspec
 
